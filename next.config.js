b513b2437ea7ad4c9e4f8b24c8e028f2c0152369
ayugithasari18-crypto <ayugithasari18@gmail.com>
2025-11-03 13:02:35 +0700

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Memastikan trailing slash dipertahankan, penting untuk routing yang konsisten
+    trailingSlash: true,
+};
+
+module.exports = nextConfig;
